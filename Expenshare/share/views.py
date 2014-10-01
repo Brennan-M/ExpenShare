cd8a3a56e898752303a02d1a13e442b78bb15b8f
Ian Char <ianchar4@gmail.com>
2014-09-30 21:55:24 -0600

    Created basic login template page

--- Expenshare/share/views.py
+++ Expenshare/share/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello World!")
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('login.html', context_dict, context)
