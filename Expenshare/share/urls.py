c25d19ea9b8a6d7115e40f69e91b20ea6f1051a2
Ian Char <ianchar4@gmail.com>
2014-10-12 16:54:39 -0600

    Linked the two pages

--- Expenshare/share/urls.py
+++ Expenshare/share/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 from share import views
 
 urlpatterns = patterns(' ',
-	url(r'^$', views.index, name='index'))
+	url(r'^$', views.index, name='index'),
+	url (r'^home/$', views.home, name='home'))
--- Expenshare/share/views.py
+++ Expenshare/share/views.py
@@ -16,4 +16,18 @@ def index(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
+    return render_to_response('login.html', context_dict, context)
+
+def home(request):
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
     return render_to_response('home.html', context_dict, context)
