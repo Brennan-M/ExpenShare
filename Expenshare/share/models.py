7cc6f27200d068ddb1731f4e20912b4b640adb08
Ian Char <ianchar4@gmail.com>
2014-10-15 13:49:29 -0600

    Tried to add some databases (untested!)

--- Expenshare/share/models.py
+++ Expenshare/share/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import Group
+# Django 1.7 has its own built in group thing so probably actually do not need this.
+# That being said it needs additional things in it so we will create a new model
+class PayGroup(models.model):
+	#Django should create unique primary key already
+	name = OneToOneField(Group)
+	description = models.CharField(max_length=400)
+	members = ManyToManyField(User)
 
-# Create your models here.
+	def __unicode__(self):
+		return self.name
+
+class PaymentLog(models.model):
+	#Django should create unique primary key already
+	amount = models.IntegerField(default=0)
+	description = models.CharField(max_length=400)
+	date = models.DateField()
+	group = models.ForeignKey(Group)
+	user = models.ForeignKey(User)
+
+	#For other features may need...
+	# contested : boolean, contestedMessage : CharField
+	def __unicode__(self):
+		return self.name
+
