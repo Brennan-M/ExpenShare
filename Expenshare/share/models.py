51f486345a3948715e472b2741270f9adbeefb50
Ian Char <ianchar4@gmail.com>
2014-10-22 16:13:07 -0600

    Tried to create DBs

--- Expenshare/share/models.py
+++ Expenshare/share/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+
 # Django 1.7 has its own built in group thing so probably actually do not need this.
 # That being said it needs additional things in it so we will create a new model
-class PayGroup(models.model):
+class PayGroup(models.Model):
 	#Django should create unique primary key already
-	name = OneToOneField(Group)
+	name = models.OneToOneField(Group)
 	description = models.CharField(max_length=400)
-	members = ManyToManyField(User)
+	members = models.ManyToManyField(User)
 
 	def __unicode__(self):
 		return self.name
 
-class PaymentLog(models.model):
+class PaymentLog(models.Model):
 	#Django should create unique primary key already
 	amount = models.IntegerField(default=0)
 	description = models.CharField(max_length=400)
