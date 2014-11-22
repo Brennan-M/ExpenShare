3d30db898949105c993968c91f3e4f53b9afd3b9
Taylor Andrews <andrewstaylorj@gmail.com>
2014-11-22 13:01:52 -0700

    working read from database

--- Expenshare/share/views.py
+++ Expenshare/share/views.py
@@ -19,7 +19,7 @@ def index(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('login.html', context_dict, context)
+    return render_to_response('home.html', context_dict, context)
 
 def home(request):
     # Request the context of the request.
