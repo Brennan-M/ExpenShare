e1a63dad8ca796a1a078b649daecce9c77592968
Taylor Andrews <andrewstaylorj@gmail.com>
2014-11-22 13:27:18 -0700

    fixed merge conflicts and updated read from database

--- Expenshare/share/views.py
+++ Expenshare/share/views.py
@@ -19,13 +19,11 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    paygroup_list = PayGroup.objects.order_by('name')
-    context_dict = {'paygroups': paygroup_list}
-
+    context_dict = {}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('home.html', context_dict, context)
+    return render_to_response('login.html', context_dict, context)
 
 def home(request):
     # Request the context of the request.
@@ -34,7 +32,8 @@ def home(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {}
+    paygroup_list = PayGroup.objects.order_by('name')
+    context_dict = {'paygroups': paygroup_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
