05896586cea22c9a88e04eed893b8bbdcd8f6f22
Taylor Andrews <andrewstaylorj@gmail.com>
2014-11-22 16:36:12 -0700

    modifed views.py context_dict

--- Expenshare/share/views.py
+++ Expenshare/share/views.py
@@ -17,42 +17,40 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    paygroup_list = PayGroup.objects.order_by('name')
-    context_dict = {'paygroups': paygroup_list}
+    context_dict = {}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render_to_response('login.html', context_dict, context)
 
-@login_required
 def home(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
+    # Request the context of the request.                                                            
+    # The context contains information such as the client's machine details, for example.            
     context = RequestContext(request)
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {}
+    # Construct a dictionary to pass to the template engine as its context.                          
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!                     
+    paygroup_list = PayGroup.objects.order_by('name')
+    context_dict = {'paygroups': paygroup_list}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Return a rendered response to send to the client.                                              
+    # We make use of the shortcut function to make our lives easier.                                     # Note that the first parameter is the template we wish to use.                                   
     return render_to_response('home.html', context_dict, context)
 
 @login_required
 def history(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
+    # Request the context of the request.                                                            
+    # The context contains information such as the client's machine details, for example.            
     context = RequestContext(request)
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {}
+    # Construct a dictionary to pass to the template engine as its context.                          
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!                     
+    paylog_list = PaymentLog.objects.order_by('group')
+    context_dict = {'paylog': paylog_list}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Return a rendered response to send to the client.                                              
+    # We make use of the shortcut function to make our lives easier.                                     # Note that the first parameter is the template we wish to use.                                   
     return render_to_response('ExpenseLog.html', context_dict, context)
 
 #View for registering new users
