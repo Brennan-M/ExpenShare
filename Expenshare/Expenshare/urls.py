84030d5ac2d252126385ba942eedc087323f55a7
Ian Char <ianchar4@gmail.com>
2014-11-22 16:34:50 -0700

    Added admin GUI

--- Expenshare/Expenshare/urls.py
+++ Expenshare/Expenshare/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'Expenshare.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^share/', include('share.urls')),
 )
--- Expenshare/share/admin.py
+++ Expenshare/share/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from share.models import PayGroup, PaymentLog
 # Register your models here.
+
+admin.site.register(PayGroup)
+admin.site.register(PaymentLog)
